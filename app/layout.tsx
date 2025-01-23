244ef483830f13cac923519f3168a023e60738c9
Luka Kechexmadze <145994802+MLYTC1@users.noreply.github.com>
2025-01-23 07:41:13 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
+  title: "Luka's Portfolio",
   description: "Modern & Minimal JS Mastery Portfolio",
 };
 
